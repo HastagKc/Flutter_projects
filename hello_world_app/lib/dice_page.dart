6d63fc8d84e5b649ebfe933ba52bc2679bd79c09
HastagKc <chaudharykshittiz950@gmail.com>
2023-06-14 22:10:37 +0545

    dice app is complete

--- hello_world_app/lib/dice_page.dart
+++ hello_world_app/lib/dice_page.dart
@@ -1,24 +1,36 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
-class DicePage extends StatelessWidget {
-  const DicePage({super.key});
+final rondomrizer = Random();
 
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
   // creating list of images
+  List<String> diceImages = [
+    'assets/diceimages/dice1.png',
+    'assets/diceimages/dice2.png',
+    'assets/diceimages/dice3.png',
+    'assets/diceimages/dice4.png',
+    'assets/diceimages/dice5.png',
+    'assets/diceimages/dice6.png',
+  ];
+
+  // creating function
+  int currentDiceIndex = 2;
+  void rollDiceFun() {
+    setState(() {
+      currentDiceIndex = rondomrizer.nextInt(6);
+      print(currentDiceIndex);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    List<String> diceImages = [
-      'assets/diceimages/dice1.png',
-      'assets/diceimages/dice2.png',
-      'assets/diceimages/dice3.png',
-      'assets/diceimages/dice4.png',
-      'assets/diceimages/dice5.png',
-      'assets/diceimages/dice6.png',
-    ];
-
-    // creating function
-
-    void rollDiceFun() {}
     return Scaffold(
       body: Center(
         child: Column(
@@ -29,46 +41,28 @@ class DicePage extends StatelessWidget {
               width: 100,
               decoration: BoxDecoration(
                 image: DecorationImage(
-                  image: AssetImage(diceImages[0]),
+                  image: AssetImage(diceImages[currentDiceIndex]),
                 ),
               ),
             ),
-            CustomButton(
-              text: 'Roll Dice',
+            ElevatedButton(
               onPressed: rollDiceFun,
+              style: ElevatedButton.styleFrom(
+                foregroundColor: const Color.fromARGB(255, 8, 1, 112),
+                backgroundColor: const Color.fromARGB(255, 28, 217, 230),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              child: const Text(
+                'Roll Dice',
+                style: TextStyle(fontSize: 16),
+              ),
             ),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class CustomButton extends StatelessWidget {
-  final String text;
-  final VoidCallback onPressed;
-
-  const CustomButton({
-    required this.text,
-    required this.onPressed,
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: onPressed,
-      style: ElevatedButton.styleFrom(
-        foregroundColor: const Color.fromARGB(255, 8, 1, 112),
-        backgroundColor: const Color.fromARGB(255, 28, 217, 230),
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(8),
-        ),
-      ),
-      child: Text(
-        text,
-        style: const TextStyle(fontSize: 16),
       ),
     );
   }
